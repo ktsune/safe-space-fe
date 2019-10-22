92a2171c5717746f66df4a046e4c4a09b111cac0
Ann Cerveny <cervann11@gmail.com>
2019-10-21 23:48:18 -0600

    Add SplashPage to toggle w/ LogInForm per isLoading

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,27 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import SplashPage from '../SplashPage/SplashPage'
+import LogInForm from '../LogInForm/LogInForm'
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      reliefCenters: [],
+      isLoading: true
+    }
+  }
+
+  //add componentDidMount to fetch relief center data
+  // once fetched, set isLoading to false
+
+  render() {
+    return (
+      <section className="App">
+        {this.state.isLoading ? <SplashPage /> : <LogInForm />}
+      </section>
+    )
+  }
 }
 
 export default App;
