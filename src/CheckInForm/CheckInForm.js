3b17ae1c757467c40e6092481b3212c9ca5b4b67
Ann Cerveny <cervann11@gmail.com>
2019-10-23 14:45:52 -0600

    Add routes to App and LogInForm
    Co-authored-by: Jacob Ogren <jOgren@gmail.com>

--- src/CheckInForm/CheckInForm.js
+++ src/CheckInForm/CheckInForm.js
@@ -1,37 +1,43 @@
-import React, { Component } from 'react'
-import ExtraItems from '../ExtraItems/ExtraItems'
+import React, { Component } from "react";
+import ExtraItems from "../ExtraItems/ExtraItems";
 
 class CheckInForm extends Component {
-    constructor() {
-        super(); 
-        this.state = {
-            firstName: "", 
-            lastName: "", 
-            age: "", 
-            items: 
-             ["baby wipes", "formula", "diapers", "breast feeding device", "feminine hygiene products", "android phone charger", "apple phone charger"]
-        }
-    }
-    render() {
+  constructor() {
+    super();
+    this.state = {
+      firstName: "",
+      lastName: "",
+      age: "",
+      items: [
+        "baby wipes",
+        "formula",
+        "diapers",
+        "breast feeding device",
+        "feminine hygiene products",
+        "android phone charger",
+        "apple phone charger"
+      ]
+    };
+  }
+  render() {
+    // let list = this.state.items.map(item => {
+    //     return <div>
+    //         <button onClick={this.toggleChecked}></button>
+    //         <p>{item.name}</p>
+    //         </div>
+    // })
+    return (
+      <section>
+        <input></input>
+        <input></input>
+        <input></input>
+        <button></button>
+        <ExtraItems items={this.state.items} />
 
-        // let list = this.state.items.map(item => {
-        //     return <div>
-        //         <button onClick={this.toggleChecked}></button>
-        //         <p>{item.name}</p>
-        //         </div>
-        // })
-        return (
-            <section>
-                <input></input>
-                <input></input>
-                <input></input>
-                <button></button>
-                <ExtraItems items={this.state.items} />
-                
-                <button>Submit</button>
-            </section>
-        )
-    }
+        <button>Submit</button>
+      </section>
+    );
+  }
 }
 
-export default CheckInForm; 
+export default CheckInForm;
---
+++ src/CheckOutForm/CheckOutForm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../CheckOutForm/CheckOutForm.css'
+
+const CheckOutForm = () => {
+  return (
+    <section className="CheckOutForm">
+      <h1>Hey!</h1>
+    </section>
+  )
+}
+
+export default CheckOutForm
