7b8aa4b3bb28e10030378422455a02ab65ea235a
Sarah Tatro <statrox4@gmail.com>
2019-10-23 11:51:19 -0600

    Add toggle form functionality to check in form

--- src/LogInForm/LogInForm.js
+++ src/LogInForm/LogInForm.js
@@ -1,46 +1,34 @@
-import React, { Component } from 'react'
-import './LogInForm.css'
-import CheckInForm from '../CheckInForm/CheckInForm';
+import React from 'react'
+// import CheckInForm from '../CheckInForm/CheckInForm'
 
-class LogInForm extends Component {
-  constructor() {
-    super();
-    this.state = {
-      isLoggedIn: true, 
-      hasBeenClicked: true
-    }
-  }
-  //Add map function to apply relief centers to dropdown menu
-  //Add function to set reliefCenterID state when clicked and toggle isLoggedIn to true
-
-  toggleForm = () => {
-    this.setState({ hasBeenClicked: !this.state.hasBeenClicked })
-  }
-
-  render() {
+const LogInForm = ({reliefCenterID, reliefCenter}) => {
+  let reliefCenterMenu = () => {
     return (
-      <section className="LogInForm">
-        {!this.state.isLoggedIn ?
-        <article className="pick-relief-center-menu">
-          <input id="dropdown-menu" placeholder="Find Relief Center..."/>
-          <button id="loginForm-submit-button">Submit</button>
-        </article>
-        :
-        <article className="navigation-menu">
-          <button id="supplies-button">Supplies</button>
-          <button id="check-in-button" onClick={this.toggleForm} >Check In</button>
-          <button id="check-out-button">Check Out</button>
-        </article>
-        }
-        {
-          this.state.hasBeenClicked 
-          && 
-          <CheckInForm /> 
-        }
-      </section>
+      <article className="pick-relief-center-menu">
+      < input id="dropdown-menu" placeholder="Find Relief Center..."/>
+        <button id="loginForm-submit-button">Submit</button>
+      </article>
     )
   }
+
+  let navigationMenu = () => {
+    return (
+      <article className="navigation-menu">
+        <button id="supplies-button">Supplies</button>
+        <button id="check-in-button" >Check In</button>
+        <button id="check-out-button">Check Out</button>
+      </article>
+    )
+  }
+
+  return (
+    <section className="LogInForm">
+    {reliefCenterID ? reliefCenterMenu() : navigationMenu()}
+    {/* {<CheckInForm /> } */}
+    </section>
+  )
 }
 
-export default LogInForm;
+export default LogInForm
+
 
