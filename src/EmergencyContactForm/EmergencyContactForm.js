d46659744b1f6e642c26bc2c64b63e64730ba64d
Ann Cerveny <cervann11@gmail.com>
2019-10-24 12:07:30 -0600

    Add inputs to BasicInfoForm component

---
+++ src/EmergencyContactForm/EmergencyContactForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EmergencyContactForm = () => {
+  return (
+    <form className="EmergencyContactForm">
+      <h1>Hello</h1>
+    </form>
+  )
+}
+
+export default EmergencyContactForm;
