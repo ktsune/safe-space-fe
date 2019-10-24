acc0a757cb02e415389e0b6adb7cc14e7c16e9d6
Sarah Tatro <statrox4@gmail.com>
2019-10-24 14:35:55 -0600

    Create Emergency Contact form for new visitor

--- src/EmergencyContactForm/EmergencyContactForm.js
+++ src/EmergencyContactForm/EmergencyContactForm.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import './EmergencyContactForm.css'
 
 const EmergencyContactForm = () => {
   return (
     <form className="EmergencyContactForm">
-      <h1>Hello</h1>
+      <h1>Emergency Contact</h1>
+
+       <label for="Full Name">Name:</label>
+       <input id="fullname" type="text" placeholder="Add Full Name"/>
+
+       <label for="phone">Phone:</label>
+       <input id="phone" type='text' placeholder="Add Phone Number"/>
+
+       <label for="relationship">Relationship:</label>
+       <input id="relationship" type='text' placeholder="Add relationship"/> 
     </form>
   )
 }
