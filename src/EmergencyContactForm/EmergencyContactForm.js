34863a14182784f8533858d279a8d36981d581c1
Ann Cerveny <cervann11@gmail.com>
2019-10-24 21:02:42 -0600

    Establish consistency among EmergencyContactForm inputs

--- src/EmergencyContactForm/EmergencyContactForm.js
+++ src/EmergencyContactForm/EmergencyContactForm.js
@@ -23,17 +23,17 @@ class EmergencyContactForm extends React.Component {
       <h1>Emergency Contact</h1>
 
       <div id='field'> 
-       <label for="Full Name">Name:</label>
+       <label id="fullname-label"for="Full Name">Name:</label>
        <input id="fullname" type="text" name='name' placeholder="Add Full Name" onChange={this.formUpdate}/>
       </div>
 
       <div id='field'> 
-        <label for="phone">Phone:</label>
+        <label id="phone-label" for="phone">Phone:</label>
         <input id="phone" type='text' name='phone' placeholder="Add Phone Number" onChange={this.formUpdate}/>
       </div>
 
       <div id='field'> 
-       <label for="relationship">Relationship:</label>
+       <label id="relationship-label" for="relationship">Relationship:</label>
        <input id="relationship" type='text' name='relationship' placeholder="Add relationship" onChange={this.formUpdate}/> 
       </div>
     </form>
