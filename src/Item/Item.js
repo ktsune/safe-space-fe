d763f54c11b0c2a5310b478b7d037a10daf85c4c
Ann Cerveny <cervann11@gmail.com>
2019-10-26 11:41:55 -0600

    Add inputs to necessary needs

--- src/Item/Item.js
+++ src/Item/Item.js
@@ -3,6 +3,9 @@ import "./Item.css";
 
 const Item = ({ key, id, item }) => {
   const [isNeeded, setIsNeeded] = useState(false);
+  const [addText, setAddText] = useState();
+
+  console.log(item);
   return (
     <article className="Item">
       <img
@@ -12,8 +15,20 @@ const Item = ({ key, id, item }) => {
           !isNeeded
             ? require("../assets/unchecked.svg")
             : require("../assets/checked.svg")
-        }/>
-      <h3 id="item-name">{item}</h3>
+        }
+        onClick={e => setIsNeeded(!isNeeded)}
+      />
+      <h3 id="item-name" onClick={e => setIsNeeded(!isNeeded)}>
+        {item}
+      </h3>
+      { item === 'dietary restrictions' && <input type="text" placeholder="Add item here" />}
+      { item === 'medications' && <input type="text" placeholder="Add item here" />}
+      { item === 'allergies' && <input type="text" placeholder="Add item here" />}
+      { item === 'other' && <input type="text" placeholder="Add item here" />}
+
+      {/* {item === "medications" || "allergies" || "other" && ( */}
+      {/* <input type="text" placeholder="Add item here" /> */}
+      {/* )} */}
     </article>
   );
 };
