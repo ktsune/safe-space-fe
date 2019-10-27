50eed83ed9b2981216fbf9235136e29ef4b42352
Ann Cerveny <cervann11@gmail.com>
2019-10-27 15:23:27 -0600

    Place correlating hooks into BasicInfForm

--- src/BasicInfoForm/BasicInfoForm.js
+++ src/BasicInfoForm/BasicInfoForm.js
@@ -2,21 +2,22 @@ import React from 'react'
 import './BasicInfoForm.css'
 
 const BasicInfoForm = ({basicInfo, setBasicInfo}) => {
-  
+  const handleChange = (e) => setBasicInfo({[e.target.name]: [e.target.value]});
+
   return (
     <form className="BasicInfoForm">
       <h1 id="basic-info-header">Basic Information:</h1>
       <div id="fullname-container">
         <label id="fullname-label" for="fullname">Full Name:</label>
-        <input id="fullname" type="text" placeholder="Add Full Name"/>
+        <input id="fullname" type="text" placeholder="Add Full Name" onChange={handleChange} name="name" value={basicInfo.name}/>
       </div>
       <div id="age-container">
         <label id="age-label" for="age">Age:</label>
-        <input id="age" type='text' placeholder="Add Age"/>
+        <input id="age" type='text' placeholder="Add Age" onChange={handleChange}/>
       </div>
       <div id="phone-container">
         <label id="phone-label" for="phone">Phone Number:</label>
-        <input id="phone" type='text' placeholder="Add Phone Number"/>
+        <input id="phone" type='text' placeholder="Add Phone Number" onChange={handleChange}/>
       </div>
     </form>
   )
