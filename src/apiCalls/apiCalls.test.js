6c59773b68014b3b4a70ef1bda47dc0e0cf417b9
Jacob Ogren <jogren0044@gmail.com>
2019-10-28 17:42:01 -0600

    Fully test apiCalls happy and sad pathes

--- src/apiCalls/apiCalls.test.js
+++ src/apiCalls/apiCalls.test.js
@@ -43,11 +43,11 @@ describe('getItems', () => {
     expect(window.fetch).toHaveBeenCalledWith('https://safe-space-be.herokuapp.com/graphql?query=query {itemsAtCenter(centerId: 2) {id name quantity}}', options);
   });
 
-  it.skip('should return an array of all projects', () => {
-    expect(getAllProjects()).resolves.toEqual(mockProjects);
+  it('should return an array of all projects', () => {
+    expect(getItems(mockCenter)).resolves.toEqual(mockItems);
   });
 
-  it.skip('should return an error if the promise resolves but the property ok isn\'t true', () => {
+  it('should return an error if the promise resolves but the property ok isn\'t true', () => {
     window.fetch = jest.fn()
       .mockImplementation(() => {
         return Promise.resolve({
@@ -55,15 +55,76 @@ describe('getItems', () => {
         })
       })
 
-    expect(getAllProjects()).rejects.toEqual(Error('There was an issue getting your projects'))
+    expect(getItems(mockCenter)).rejects.toEqual(Error('There was an error getting your items'))
   });
 
-  it.skip('should return an error if the promise rejects', () => {
+  it('should return an error if the promise rejects', () => {
     window.fetch = jest.fn()
       .mockImplementation(() => {
-        return Promise.reject(Error('Failed to fetch projects'))
+        return Promise.reject(Error('Failed to fetch items'))
       })
 
-    expect(getAllProjects()).rejects.toEqual(Error('Failed to fetch projects'))
+    expect(getItems(mockCenter)).rejects.toEqual(Error('Failed to fetch items'))
+  });
+});
+
+describe('patchItem', () => {
+  let mockItem;
+  let mockCenter;
+  let options;
+
+  beforeEach(() => {
+    options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+    mockCenter = {
+      name: 'Turing Relief',
+      id: 2
+    }
+    mockItem = {
+        id: 1,
+        name: 'diapers',
+        quantity: 5
+    }
+
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve(mockItem)
+      })
+    })
+  });
+
+  it('should call fetch with the correct url', () => {
+    patchItem(mockItem, mockCenter);
+
+    expect(window.fetch).toHaveBeenCalledWith('https://safe-space-be.herokuapp.com/graphql?query=mutation {editItem(id: 1, centerId: 2, quantity: 5) {item {quantity}}}', options);
+  });
+
+  it('should return an array of all projects', () => {
+    expect(patchItem(mockItem, mockCenter)).resolves.toEqual(mockItem);
+  });
+
+  it('should return an error if the promise resolves but the property ok isn\'t true', () => {
+    window.fetch = jest.fn()
+      .mockImplementation(() => {
+        return Promise.resolve({
+          ok: false
+        })
+      })
+
+    expect(patchItem(mockItem, mockCenter)).rejects.toEqual(Error('There was an error editing your item'))
+  });
+
+  it('should return an error if the promise rejects', () => {
+    window.fetch = jest.fn()
+      .mockImplementation(() => {
+        return Promise.reject(Error('Failed to edit item'))
+      })
+
+    expect(patchItem(mockItem, mockCenter)).rejects.toEqual(Error('Failed to edit item'))
   });
 });
