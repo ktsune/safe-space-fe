d954d47a2090b9f1b8d3d609ee82324c0c8460d6
Sarah Tatro <statrox4@gmail.com>
2019-10-28 18:10:23 -0600

    Use fetch & a mutation to delete a user from the database

--- src/CheckOutForm/CheckOutForm.js
+++ src/CheckOutForm/CheckOutForm.js
@@ -1,17 +1,51 @@
 import React, { useState, useContext } from 'react';
 import { UsersContext } from '../Contexts/UsersContext';
+import { useMutation } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
 import '../CheckOutForm/CheckOutForm.css';
-
 
 const CheckOutForm = () => {
   const { currentUsers, setCurrentUsers } = useContext(UsersContext)
   const [search, updateSearch] = useState('');  
-  
+
+  const deleteUserFromDB = async (id) => {
+    let options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+
+    let queryParams = `mutation {deleteUser(id: ${id}) {user {name}}}`
+
+    let url = `https://safe-space-be.herokuapp.com/graphql?query=${queryParams}`
+
+    try {
+      let resp = await fetch(url, options);
+
+      if (!resp.ok) {
+        throw new Error('There was an error deleting the user')
+      }
+
+      let data = await resp.json();
+      return data
+
+    } catch (error) {
+      throw error
+    }
+  }  
+
+  const deleteUser = async (id) => {
+    await deleteUserFromDB(id)
+    let filteredUsers = currentUsers.original.filter(person => person.id != 3)
+    setCurrentUsers({ result: filteredUsers, original: filteredUsers })
+  }
+
   let namesArray = !search ? currentUsers.original : currentUsers.result 
   let namesList = namesArray.map((name, index) => {
     return <div className="CheckOutForm_visitor button push" id="push" key={index}>
       <p>Check {name.name} Out </p>
-      <button id='delete-visitor'>X</button>
+      <button onClick={() => deleteUser(name.id)} id='delete-visitor'>X</button>
     </div>
   })
   
